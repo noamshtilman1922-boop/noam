4b201c9bc1e0b846e8d9c3990b8c3c4a5829b377
Noam Shtilman <noamshtilman1922@gmail.com>
2025-12-23 13:26:02 +0200

    יח

---
+++ noam/JavaScript.js
@@ -0,0 +1,28 @@
+﻿alert("hello heli");
+document.write("helicopter");
+var num = prompt("Enter a number:", 7);
+document.write(num);
+var num = parseInt(prompt("Enter a number:"));
+var name = prompt("Enter your name:", "");
+document.write("<br />");
+document.write("Hello " + name);
+var age = parseInt(prompt("Enter your age:", 0));
+document.write("you are " + age + " years old")
+document.write("<br />");
+var age1 = age + 1;
+document.write("You will be " + age1 + " years old next year");
+
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a number:"));
+var sum = num1 + num2;
+var divide = num1 / num2;
+var hefresh = num1 - num2;
+var multiply = num1 * num2;
+document.write("<br />");
+document.write("sum:" + sum);
+document.write("<br />");
+document.write("divide:" + divide);
+document.write("<br />");
+document.write("hefresh:" + hefresh);
+document.write("<br />");
+document.write("multiply:" + multiply);
